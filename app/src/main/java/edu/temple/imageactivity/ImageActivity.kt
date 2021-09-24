8041fba4186d33ce5ab725c1a1a8ff4c817e88e8
Saimon Shrestha <sai.shrestha14@gmail.com>
2021-09-24 19:12:31 -0400

    Changed names of images from uppercase to lowercase. Added a recyler_layout.xml Added an ImageAdapter

--- app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
@@ -2,10 +2,20 @@ package edu.temple.imageactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recView = findViewById<RecyclerView>(R.id.recyclerView)
+        val imgView = findViewById<ImageView>(R.id.imageView)
+        val imgDesc = findViewById<TextView>(R.id.imageDesc)
+        recView.layoutManager = GridLayoutManager(this,4)
     }
 }
